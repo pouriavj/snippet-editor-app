950f3c5c7521c2cc48422d725eef1eb7a1c16708
Pouria <108181964+pouriavj@users.noreply.github.com>
2026-05-24 23:28:56 +0330

    Add Responsive Save Button

--- src/components/icons/save-icon.tsx
+++ src/components/icons/save-icon.tsx
@@ -1,9 +1,8 @@
 export default function SaveIcon() {
   return (
     <svg
+      className="save-icon"
       xmlns="http://www.w3.org/2000/svg"
-      width="28"
-      height="28"
       viewBox="0 0 36 36"
     >
       <path
